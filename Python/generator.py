079818f85cba3f75d7cd536dff4296376b56b2d6
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-06-16 11:27:46 +0600

    Python Generator

---
+++ Python/generator.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3.6
+
+
+def is_prime(n):
+    if n == 1:
+        return False
+    for x in range(2, n):
+        if n % x == 0:
+            return False
+        else:
+            return True
+
+
+def primes(n, last):
+    while n < last:
+        if is_prime(n):
+            yield n
+        n += 1
+
+
+for n in primes(1, 70):
+    # if n > 100:
+    #     break
+    print(n)
