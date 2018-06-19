4f5a5310312886897d53aa05cdcd2cf70a4f0478
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-06-19 20:45:42 +0600

    Python regular expression

---
+++ Python/regular-expression.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3.6
+
+import re
+
+
+def main():
+    project = 0
+    promise = 0
+    lines = open('wap.txt', 'r')
+    for line in lines:
+        match = re.search('pro(ject|mise)', line)
+        if match:
+            group = match.group()
+            if group == 'project':
+                project += 1
+            else:
+                promise += 1
+    print("Promise: {} | Project: {}".format(promise, project), end='\t')
+
+
+if __name__ == "__main__": main()
