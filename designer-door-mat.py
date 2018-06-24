eddc5716e48e16960284ba933c9b759dceae1da7
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-06-24 11:11:56 +0600

    Designer Door Mat

---
+++ designer-door-mat.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3.6
+
+
+def main():
+    (N, M) = map(int, input().split())
+    center = N // 2
+
+    for i in range(center):
+        print((".|." * (2 * i + 1)).center(M, '-'))
+
+    print('WELCOME'.center(M, '-'))
+
+    for i in reversed(range(center)):
+        print((".|." * (2 * i + 1)).center(M, '-'))
+
+
+if __name__ == '__main__':
+    main()
---
+++ python-lists.py
@@ -0,0 +1,30 @@
+#!/usr/local/bin/python3.6
+
+
+def main():
+    n = int(input())
+    output = []
+    actions = []
+
+    for _ in range(n):
+        actions.append(input().split())
+
+    for action in actions:
+        if action[0] == 'insert':
+            output.insert(int(action[1]), int(action[2]))
+        elif action[0] == 'remove':
+            output.remove(int(action[1]))
+        elif action[0] == 'append':
+            output.append(int(action[1]))
+        elif action[0] == 'sort':
+            output.sort()
+        elif action[0] == 'pop':
+            output.pop()
+        elif action[0] == 'reverse':
+            output.reverse()
+        else:
+            print(output)
+
+
+if __name__ == '__main__':
+    main()
