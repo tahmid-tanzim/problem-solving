e8030911215fefbc285c84f7a0c38e5c98728f47
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-06-26 11:55:39 +0600

    Alphabet Rangoli Done

--- alphabet-rangoli.py
+++ alphabet-rangoli.py
@@ -1,18 +1,35 @@
 #!/usr/local/bin/python3.6
 
+import string
+
+
+def print_rangoli(size):
+    width = 4 * size - 3
+    alphabet = string.ascii_lowercase
+    lines = []
+    i = size - 1
+    while i >= 0:
+        right = alphabet[i:size]
+        left = right[::-1][:-1]
+        full_txt = '-'.join(list(left + right)).center(width, '-')
+        if i != 0:
+            lines.insert(0, full_txt)
+        print(full_txt)
+        i -= 1
+
+    for line in lines:
+        print(line)
+
 
 def main():
-    (N, M) = map(int, input().split())
-    center = N // 2
-
-    for i in range(center):
-        print((".|." * (2 * i + 1)).center(M, '-'))
-
-    print('WELCOME'.center(M, '-'))
-
-    for i in reversed(range(center)):
-        print((".|." * (2 * i + 1)).center(M, '-'))
+    for i in (1, 3, 5, 10):
+        print_rangoli(i)
 
 
 if __name__ == '__main__':
     main()
+
+
+# 3 -> 9
+# 5 -> 17
+# 10 -> 37
