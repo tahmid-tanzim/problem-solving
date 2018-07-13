cbf892fa5c1f54ba2c40a0009b3c98fa2d53340f
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2018-07-13 20:21:25 +0600

    Try Selection Sort DESC order in Python 3

---
+++ Python/bubble-sort.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3.6
+
+
+def bubble_sort(A, n):
+    return n
+
+
+if __name__ == "__main__":
+    output = bubble_sort([3, 44, 38, 5, 47, 15, 36, 26, 27, 2], 10)
+    print(output)
--- Python/selection-sort.py
+++ Python/selection-sort.py
@@ -1,21 +1,21 @@
 #!/usr/local/bin/python3.6
 
 
-def selection_sort(A, n):
+def selection_sort(A, n, order='ASC'):
     for i in range(n - 1):
-        index_min = i
+        index = i
         for j in range(i + 1, n):
-            if A[j] < A[index_min]:
-                index_min = j
+            if A[j] < A[index] and order == 'ASC' or A[j] > A[index] and order == 'DESC':
+                index = j
 
         # Swap with minimum value
-        if index_min != i:
-            A[i] = A[i] + A[index_min]
-            A[index_min] = A[i] - A[index_min]
-            A[i] = A[i] - A[index_min]
+        if index != i:
+            A[i] = A[i] + A[index]
+            A[index] = A[i] - A[index]
+            A[i] = A[i] - A[index]
     return A
 
 
 if __name__ == "__main__":
-    output = selection_sort([3, 44, 38, 5, 15, 26, 27, 2, 46, 5, 4], 11)
+    output = selection_sort([3, 44, 38, 5, 15, 26, 27, 2, 46, 5, 4], 11, 'DESC')
     print(output)
