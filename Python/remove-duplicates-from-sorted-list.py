f2133f5f2521ee76cd41901874d9a7df07d8c3a4
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2019-07-07 12:40:13 +0600

    Create linked list from python list array

--- Python/remove-duplicates-from-sorted-list.py
+++ Python/remove-duplicates-from-sorted-list.py
@@ -8,51 +8,29 @@ class ListNode:
         self.next = None
 
 
-def remove_elements(head, val):
-    if head is None:
-        return None
-
-    # Remove from head
-    while head.val == val:
-        if head.next is not None:
-            head = head.next
+def delete_duplicates(head):
+    h = head
+    while h.next is not None:
+        if h.val == h.next.val:
+            h.next = h.next.next if h.next.next is not None else None
         else:
-            return None
-
-    i = head
-    while i.next is not None:
-        if i.next.val == val:
-            i.next = i.next.next if i.next.next is not None else None
-        else:
-            i = i.next
-    return head
+            h = h.next
+    return h
 
 
 if __name__ == '__main__':
-    l1 = ListNode(1)
-    l2 = ListNode(2)
-    l3 = ListNode(3)
-    l4 = ListNode(4)
-    l5 = ListNode(5)
-    l6 = ListNode(6)
-    l66 = ListNode(6)
-    l666 = ListNode(6)
-    l6666 = ListNode(6)
+    # [1, 1, 2]  || [1, 1, 2, 3, 3]
 
-    # l6.next = l1
-    # l1.next = l2
-    # l2.next = l66
-    # l66.next = l3
-    # l3.next = l666
-    # l666.next = l4
-    # l4.next = l5
-    # l5.next = l6666
+    node_head = None
+    pointer = ListNode(None)
+    for i in [1, 1, 2, 3, 3]:
+        pointer.next = ListNode(i)
+        if node_head is None:
+            node_head = pointer.next
+        pointer = pointer.next
 
-    l6.next = l66
-    l66.next = l666
-
-    j = remove_elements(l6, 6)
-    # j = l6
-    while j is not None:
-        print(j.val)
-        j = j.next
+    j = delete_duplicates(node_head)
+    # j = head
+    # while j is not None:
+    #     print(j.val)
+    #     j = j.next
