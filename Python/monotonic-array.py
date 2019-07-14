8e513c3161c08e37a707ab6434ffd03789338f94
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2019-07-14 20:37:42 +0600

    Monotonic Array

---
+++ Python/monotonic-array.py
@@ -0,0 +1,15 @@
+#!/Users/tahmid.tanzim/venv/bin/python3.7
+# https://leetcode.com/problems/monotonic-array/
+
+
+def is_monotonic(A):
+    flag = True
+    i = 1
+    while i < len(A):
+
+        i += 1
+    return flag
+
+
+if __name__ == '__main__':
+    print(is_monotonic([1, 2, 2, 3]))
