c9e4df6677de20afd15abb574145854c6ef3bb87
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2019-09-25 12:12:06 +0600

    Remove vowels from a string

---
+++ Python/remove-vowels.py
@@ -0,0 +1,15 @@
+#!/Users/tahmid.tanzim/venv/bin/python3.7
+# https://leetcode.com/problems/remove-vowels-from-a-string/
+
+
+def remove_vowels(S):
+    output, vowels = "", ('a', 'e', 'i', 'o', 'u')
+    for s in S:
+        if s not in vowels:
+            output += s
+    return output
+
+
+if __name__ == '__main__':
+    print(remove_vowels("leetcodeisacommunityforcoders"))
+    print(remove_vowels("aeiou"))
