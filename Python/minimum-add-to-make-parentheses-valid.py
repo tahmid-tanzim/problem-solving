056994d074b8f5b7828f1e7c627bfa5627db89bc
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-05-16 23:42:38 +0600

    LeetCode - 921. Minimum Add to Make Parentheses Valid

---
+++ Python/minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python3.6
+
+"""
+https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+"""
+
+
+def min_add_to_make_valid(S):
+    stack = list()
+    counter = 0
+    for char in S:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if len(stack) > 0:
+                stack.pop(-1)
+            else:
+                counter += 1
+    return counter + len(stack)
+
+
+if __name__ == '__main__':
+    print(min_add_to_make_valid("())"))
+    print(min_add_to_make_valid("((("))
+    print(min_add_to_make_valid("()"))
+    print(min_add_to_make_valid("()))(("))
+
+    # _test = list()
+    # _test.append('a')
+    # _test.append('b')
+    # _test.append('c')
+    # print(_test)
+    # last = _test.pop(-1)
+    # print(_test, last)
