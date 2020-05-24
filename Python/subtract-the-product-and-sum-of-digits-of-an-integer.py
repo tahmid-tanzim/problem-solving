d149692f22ab770257c55c4fcbab24a360b115ad
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-05-24 21:58:16 +0600

    LeetCode - 1281. Subtract the Product and Sum of Digits of an Integer

---
+++ Python/subtract-the-product-and-sum-of-digits-of-an-integer.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3.6
+
+"""
+https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+"""
+
+
+def subtract_product_and_sum(n):
+    product = 1
+    add = 0
+    while n != 0:
+        i = n % 10
+        add += i
+        product *= i
+        n = n // 10
+    return product - add
+
+
+if __name__ == '__main__':
+    print(subtract_product_and_sum(234))
+    print(subtract_product_and_sum(4421))
+
