15b91b674b862f91ad385a6f852703fe5a3d6232
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-05-24 18:02:26 +0600

    LeetCode - 1389. Create Target Array in the Given Order

---
+++ Python/create-target-array-in-the-given-order.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3.6
+
+"""
+https://leetcode.com/problems/create-target-array-in-the-given-order/
+"""
+
+
+def create_target_array(nums, index):
+    target = list()
+    for i in range(len(nums)):
+        target = target[:index[i]] + [nums[i]] + target[index[i]:]
+    return target
+
+
+if __name__ == '__main__':
+    print(create_target_array(nums=[0, 1, 2, 3, 4], index=[0, 1, 2, 2, 1]))
+    print(create_target_array(nums=[1, 2, 3, 4, 0], index=[0, 1, 2, 3, 0]))
+    print(create_target_array(nums=[1], index=[0]))
