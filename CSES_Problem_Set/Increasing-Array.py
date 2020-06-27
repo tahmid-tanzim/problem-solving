2a76bfbb90997f82461e8433668f0be69227ad14
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-06-27 19:49:58 +0600

    CSES Problem Set - Increasing Array

---
+++ CSES_Problem_Set/Increasing-Array.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python3
+# https://cses.fi/problemset/task/1094
+
+if __name__ == '__main__':
+    n = int(input())
+    array = list(map(int, input().split()))
+    diff, i = 0, 1
+    while i < n:
+        d = array[i - 1] - array[i]
+        if d > 0:
+            array[i] += d
+            diff += d
+        i += 1
+    print(diff)
+
