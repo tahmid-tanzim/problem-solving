2fad45705a9a5fd1f5b8e90cbf9aaeb0ba535290
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-07-07 03:35:37 +0600

    CSES.FI Trailing Zeros

---
+++ CSES_Problem_Set/Trailing-Zeros.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+# https://cses.fi/problemset/task/1618
+
+if __name__ == '__main__':
+    n = int(input())
+    zeros = 0
+    i = 5
+    while n / i >= 1:
+        zeros += int(n / i)
+        i *= 5
+    print(zeros)
