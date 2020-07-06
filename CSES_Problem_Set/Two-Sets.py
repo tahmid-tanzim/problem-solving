c9680af8167a0460d81a58162bb630763a6e4d2c
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-07-07 03:07:17 +0600

    CSES - Two Sets

---
+++ CSES_Problem_Set/Two-Sets.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python3
+# https://cses.fi/problemset/task/1092
+
+if __name__ == '__main__':
+    n = int(input())
+    _sum = int(n * (n+1) / 2)
+
+    if _sum % 2 == 0:
+        print('YES')
+        _sum /= 2
+        _set = [[], []]
+        for i in range(n, 0, -1):
+            if i <= _sum:
+                _set[0].append(i)
+                _sum -= i
+            else:
+                _set[1].append(i)
+        print(len(_set[0]))
+        print(*_set[0])
+        print(len(_set[1]))
+        print(*_set[1])
+    else:
+        print('NO')
+
+
