23585f2aa68784505d7873022cf1e30080ffb1df
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-09-13 18:05:49 +0600

    LeetCode - 416. Partition Equal Subset Sum

--- 6-type-knapsack-problem.py
+++ 6-type-knapsack-problem.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 # https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
 # https://www.youtube.com/watch?v=iBnWHZmlIyY
+# https://leetcode.com/problems/partition-equal-subset-sum
 
 """
 # Dynamic Programming
@@ -80,7 +81,88 @@ class SubsetSum:
 
 
 class EqualSubsetSumPartition:
-    pass
+    def __init__(self) -> None:
+        self.memoization_table = None
+
+    def init_memoization_table(self, row: int, col: int) -> None:
+        # row ~> n = 6
+        # col ~> total = 30
+        # n x total
+        self.memoization_table = [[None for _ in range(col + 1)] for _ in range(row + 1)]
+
+    # Method 1: Recursion
+    def is_subset_sum(self, nums: List[int], n: int, total: int) -> bool:
+        # Base Condition
+        if total == 0:
+            return True
+        if n == 0:
+            return False
+
+        # Memoization
+        if self.memoization_table[n][total] is not None:
+            return self.memoization_table[n][total]
+
+        if nums[n - 1] <= total:
+            include = self.is_subset_sum(nums, n - 1, total - nums[n - 1])
+            exclude = self.is_subset_sum(nums, n - 1, total)
+            self.memoization_table[n][total] = include or exclude
+            return self.memoization_table[n][total]
+        else:
+            exclude = self.is_subset_sum(nums, n - 1, total)
+            self.memoization_table[n][total] = exclude
+            return self.memoization_table[n][total]
+
+    def is_eq_partition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+
+        if total % 2 != 0:
+            return False
+
+        n = len(nums)
+        total //= 2
+
+        self.init_memoization_table(n, total)
+        return self.is_subset_sum(nums, n, total)
+
+    # Method 2: Top Down
+    def can_partition(self, nums: List[int]) -> bool:
+        total = sum(nums)
+
+        # Base Condition
+        if total % 2 != 0:
+            return False
+
+        n = len(nums)
+        total //= 2
+
+        # Initialize Matrix
+        matrix = [[None for _ in range(total + 1)] for _ in range(n + 1)]
+
+        r = 0
+        while r <= n:
+            matrix[r][0] = True
+            r += 1
+
+        c = 1
+        while c <= total:
+            matrix[0][c] = False
+            c += 1
+
+        # Choice Diagram
+        r = 1
+        while r <= n:
+            c = 1
+            while c <= total:
+                if nums[r - 1] <= c:
+                    include = matrix[r - 1][c - nums[r - 1]]
+                    exclude = matrix[r - 1][c]
+                    matrix[r][c] = include or exclude
+                else:
+                    matrix[r][c] = matrix[r - 1][c]
+                c += 1
+            r += 1
+
+        return matrix[n][total]
 
 
 if __name__ == "__main__":
@@ -99,7 +181,7 @@ if __name__ == "__main__":
         },
 
     )
-
+    print('\n:~: Subset Sum :~:\n')
     for i in subset_sum_inputs:
         ss_rc = SubsetSum()
         ss_rc.set_memoization_table(i['n'], i['total'])
@@ -108,5 +190,27 @@ if __name__ == "__main__":
 
         ss_td = SubsetSum()
         ss_td_o = ss_td.is_subset_sum_2(i['arr'], i['n'], i['total'])
-        print(f'Method 2: Top Down.\nExpected Output - {i["output"]}\nOriginal Output - {ss_td_o}', end='\n-----------------------\n')
+        print(f'Method 2: Top Down.\nExpected Output - {i["output"]}\nOriginal Output - {ss_td_o}',
+              end='\n-----------------------\n')
 
+    equal_subset_sum_partition_inputs = (
+        {
+            'nums': [1, 5, 11, 5],
+            'output': True,
+        },
+        {
+            'nums': [1, 2, 3, 5],
+            'output': False
+        },
+
+    )
+    print('\n:~: Equal Subset Sum Partition :~:\n')
+    for i in equal_subset_sum_partition_inputs:
+        essp_rc = EqualSubsetSumPartition()
+        essp_rc_o = essp_rc.is_eq_partition(i['nums'])
+        print(f'Method 1: Recursion.\nExpected Output - {i["output"]}\nOriginal Output - {essp_rc_o}', end='\n\n')
+
+        essp_td = EqualSubsetSumPartition()
+        essp_td_o = essp_td.can_partition(i['nums'])
+        print(f'Method 2: Top Down.\nExpected Output - {i["output"]}\nOriginal Output - {essp_td_o}',
+              end='\n-----------------------\n')
