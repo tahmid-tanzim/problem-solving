2ce528cc0bba23d72025b9150a240e3571aff789
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-09-18 10:51:29 +0600

    LeetCode - Reverse string recursion

--- reverse-string.py
+++ reverse-string.py
@@ -1,4 +1,5 @@
 #!/usr/local/bin/python3
+# https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1440/
 
 
 class Solution:
@@ -8,11 +9,25 @@ class Solution:
         """
         x = len(s)
         i = 0
-        while i < x / 2:
+        while i < x // 2:
             temp = s[i]
             s[i] = s[x - i - 1]
             s[x - i - 1] = temp
             i += 1
+        print(s)
+
+    def reverse_str_recursion(self, s, i, n):
+        if i >= n:
+            return
+        else:
+            temp = s[i]
+            s[i] = s[n]
+            s[n] = temp
+            self.reverse_str_recursion(s, i + 1, n - 1)
+            return s
+
+    def reverse_string(self, s):
+        s = self.reverse_str_recursion(s, 0, len(s) - 1)
         print(s)
 
 
@@ -20,3 +35,6 @@ if __name__ == "__main__":
     o = Solution()
     o.reverseString(["h", "e", "l", "l", "o"])
     o.reverseString(["H", "a", "n", "n", "a", "h"])
+
+    o.reverse_string(["h", "e", "l", "l", "o"])
+    o.reverse_string(["H", "a", "n", "n", "a", "h"])
