c6c4cf663744f4c7c6fef84839af3f16b469a174
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-10-23 10:44:49 +0600

    LeetCode 70 - Climbing Stairs

---
+++ climbing-stairs.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3
+# https://leetcode.com/problems/climbing-stairs/
+
+
+class Solution:
+    def __init__(self) -> None:
+        self.lookup = [-1] * 46
+
+    def climb_stairs(self, n: int) -> int:
+        if n == 1 or n == 2:
+            self.lookup[n] = n
+
+        if self.lookup[n] == -1:
+            self.lookup[n] = self.climb_stairs(n - 1) + self.climb_stairs(n - 2)
+
+        return self.lookup[n]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.climb_stairs(5))
