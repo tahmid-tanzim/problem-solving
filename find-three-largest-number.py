24c4f9e434c8171f90d42e543ef46965269171b1
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-12-07 19:12:38 +0600

    AlgoExpert - Find Three Largest Numbers

---
+++ find-three-largest-number.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3.6
+
+
+def findThreeLargestNumbers(array):
+    largest_numbers = array[0:3]
+    largest_numbers.sort()
+    i = 3
+    while i < len(array):
+        if array[i] > largest_numbers[0]:
+            largest_numbers[0] = array[i]
+            for x in range(2):
+                if largest_numbers[x] > largest_numbers[x + 1]:
+                    temp = largest_numbers[x]
+                    largest_numbers[x] = largest_numbers[x + 1]
+                    largest_numbers[x + 1] = temp
+        i += 1
+    return largest_numbers
+
+
+if __name__ == '__main__':
+    print(findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]))
