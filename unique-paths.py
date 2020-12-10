0fb7882f4c6eec9155fb1023d3808ac48cc594a7
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-12-10 12:11:01 +0600

    LeetCode - Unique Path

---
+++ unique-paths.py
@@ -0,0 +1,22 @@
+#!/usr/local/bin/python3
+
+
+def unique_paths(m: int, n: int, memo: dict = {}) -> int:
+    # Base Case
+    if m == 0 or n == 0:
+        return 0
+    if m == 1 and n == 1:
+        return 1
+
+    key = f'{m}-{n}'
+    if key in memo:
+        return memo[key]
+
+    move_down = unique_paths(m - 1, n, memo)
+    move_right = unique_paths(m, n - 1, memo)
+    memo[key] = move_down + move_right
+    return memo[key]
+
+
+if __name__ == "__main__":
+    print(unique_paths(2, 3))
