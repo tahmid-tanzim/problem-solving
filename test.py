22feabb0a9539f0f85a8b461c3dc0586db7fb70a
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-03-22 13:28:27 +0600

    AlgoExpert - (River Sizes) Breadth First Search in 2D Matrix

--- test.py
+++ test.py
@@ -1,5 +1,28 @@
 #!/usr/bin/python3
 
+
+def totalPath(node, value=0):
+    if node is None:
+        return value
+
+    left = totalPath(node.left, value + node.value)
+    right = totalPath(node.right, value + node.value)
+    return max(left, right)
+
+
+def maxPathSum(tree):
+    result = 0
+    totalLeftSum = totalPath(tree.left)
+    totalRightSum = totalPath(tree.right)
+    if tree.value > 0:
+        result += tree.value
+    if totalLeftSum > 0:
+        result += totalLeftSum
+    if totalRightSum > 0:
+        result += totalRightSum
+    return result
+
+
 if __name__ == "__main__":
     last_item = 1
     prev_permutation = [2, 3]
