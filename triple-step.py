6331ad75bb9558559c5340d9e1b52006cbb6ceeb
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-04-02 01:27:57 +0600

    Dynamic Programming - Triple Step

---
+++ triple-step.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Dynamic Programming
+def tripleStep(n):
+    if n < 0:
+        return 0
+    if n == 0:
+        return 1
+
+    return tripleStep(n - 1) + tripleStep(n - 2) + tripleStep(n - 3)
+
+
+if __name__ == "__main__":
+    print(f'Answer - {tripleStep(5)}')
