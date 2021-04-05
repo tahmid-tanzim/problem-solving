61ff31ecf359b0a307e540822d485c19a1700ae9
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-04-05 12:38:51 +0600

    AlgoExpert - First Non-Repeating Character

---
+++ first-non-repeating-character.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def firstNonRepeatingCharacter(string):
+    hashtable = dict()
+    index = 0
+    for char in string:
+        if char in hashtable:
+            hashtable[char][0] += 1
+        else:
+            hashtable[char] = [1, index]
+        index += 1
+
+    for key in hashtable:
+        if hashtable[key][0] == 1:
+            print(f'First Non-Repeating Character is {key} at index {hashtable[key][1]}')
+            return hashtable[key][1]
+
+    return -1
+
+
+if __name__ == '__main__':
+    print(f'Answer - {firstNonRepeatingCharacter("abcdcafcbdf")}')
