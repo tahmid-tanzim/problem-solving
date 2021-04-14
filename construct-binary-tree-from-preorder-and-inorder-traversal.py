a9b08415e2b60f354df1a5b92bba2390538f14fe
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-04-14 13:06:01 +0600

    Construct Binary Tree from Preorder and Inorder Traversal

---
+++ construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+# https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def constructBT(self, preorder: List[int], i: int, n: int, inorder: List[int], iStart: int, iEnd: int) -> TreeNode:
+        # Base Case
+        if iStart >= iEnd or i >= n:
+            return
+        # if iStart == iEnd - 1:
+        #     [value] = inorder[iStart:iEnd]
+        #     return TreeNode(value)
+
+        # Find Root Node
+        rootValue = None
+        for x in range(i, n):
+            if preorder[x] in inorder[iStart:iEnd]:
+                rootValue = preorder[x]
+                i = x
+                break
+
+        # if rootValue is not None:
+        rootIdx = inorder.index(rootValue)
+        root = TreeNode(rootValue)
+        root.left = self.constructBT(preorder, i, n, inorder, iStart, rootIdx)
+        root.right = self.constructBT(preorder, i, n, inorder, rootIdx + 1, iEnd)
+        return root
+
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        return self.constructBT(preorder, 0, len(preorder), inorder, 0, len(inorder))
+
+
+def order(node):
+    if node is None:
+        return
+
+    order(node.left)
+    print(node.val, end='-')
+    order(node.right)
+
+
+if __name__ == "__main__":
+    s = Solution()
+    # pre = [1, 2, 4, 8, 9, 10, 11, 5, 3, 6, 7]
+    # ino = [8, 4, 10, 9, 11, 2, 5, 1, 6, 3, 7]
+    pre = [3, 9, 20, 15, 7]
+    ino = [9, 3, 15, 20, 7]
+    tree = s.buildTree(pre, ino)
+    order(tree)
