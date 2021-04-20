aaf6464282272a47f7c0d65d7f4d34350855f31c
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-04-20 12:52:19 +0600

    Cracking the Coding Interview - isUniqueChars

---
+++ Arrays and Strings/is-unique-chars.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# CTCI - 1.1
+
+def isUniqueChars(s):
+    checker = 0
+    n = len(s)
+    for i in range(n):
+        val = ord(s[i]) - ord('a')
+        if (checker & (1 << val)) > 0:
+            print(i, s[i])
+            return False
+        checker |= (1 << val)
+    return True
+
+
+if __name__ == '__main__':
+    output1 = isUniqueChars("asdertysg")
+    print(f'{output1}')
+
+    # for i in range(1, 11):
+    #     print(i, i << i)
