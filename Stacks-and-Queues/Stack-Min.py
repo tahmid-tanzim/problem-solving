f1e5d97d782fa5ef660c5fa287dfa461dfa0cac3
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-05-01 21:23:06 +0600

    Stack Min

---
+++ Stacks-and-Queues/Stack-Min.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+from Node import Node
+from Stack import Stack
+
+
+class StackMin(Stack):
+    def __init__(self, top: Node = None):
+        self.__minValueStack = Stack()
+        super().__init__(top)
+
+    def minimum(self):
+        if self.__minValueStack.isEmpty():
+            return float('inf')
+        else:
+            return self.__minValueStack.peek()
+
+    def push(self, data: int) -> None:
+        if data < self.minimum():
+            self.__minValueStack.push(data)
+        super().push(data)
+
+    def pop(self) -> Node:
+        data = super().pop()
+        if data == self.minimum():
+            self.__minValueStack.pop()
+        return data
+
+
+if __name__ == "__main__":
+    sm = StackMin()
+    sm.push(5)
+    sm.push(6)
+    sm.push(3)
+    sm.push(7)
+    sm.pop()
+    sm.pop()
+    print(f'Top - {sm.peek()} | Min - {sm.minimum()}')
