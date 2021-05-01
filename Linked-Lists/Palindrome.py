b4d9ad1a9964d407a7b632f3eecab069105c4389
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-05-01 17:19:43 +0600

    LinkedList Palindrome - Reverse and Compare & Iterative Approach using Stack

--- Linked-Lists/Palindrome.py
+++ Linked-Lists/Palindrome.py
@@ -2,21 +2,69 @@
 """
 LinkedList - 2.6. Palindrome
 """
-from LinkedList import LinkedList, Node
+from typing import List
+from LinkedList import Node
 
 
 class ReverseAndCompare:
-    def __init__(self, head=None):
-        self.head = head
+    """
+    Solution 1
+    O(n) time
+    O(n) space
+    """
+    def __init__(self, h=None):
+        self.head = h
 
-    def reverseAndClone(self):
-        pass
+    def _reverseAndClone(self) -> Node:
+        current_node = self.head
+        reversed_head: Node = None
+        while current_node is not None:
+            node: Node = Node(current_node.data)
+            node.next = reversed_head
+            reversed_head = node
+            current_node = current_node.next
+        return reversed_head
 
-    def isEqual(self):
-        pass
+    def isPalindrome(self) -> bool:
+        current_node = self.head
+        reversed_node: Node = self._reverseAndClone()
+        while current_node is not None and reversed_node is not None:
+            if current_node.data != reversed_node.data:
+                return False
+            current_node = current_node.next
+            reversed_node = reversed_node.next
 
-    def isPalindrome(self):
-        pass
+        return current_node is None and reversed_node is None
+
+
+class IterativeApproach:
+    """
+    Solution 2 using stack
+    O(n) time
+    O(n) space
+    """
+    def __init__(self, h=None):
+        self.head = h
+
+    def isPalindrome(self) -> bool:
+        fast = slow = self.head
+        stack: List[int] = list()
+        while fast is not None and fast.next is not None:
+            stack.append(slow.data)
+            slow = slow.next
+            fast = fast.next.next
+
+        # By pass middle node of odd length linkedList
+        if fast is not None:
+            slow = slow.next
+
+        while slow is not None:
+            val = stack.pop()
+            if slow.data != val:
+                return False
+            slow = slow.next
+
+        return True
 
 
 if __name__ == "__main__":
@@ -43,25 +91,25 @@ if __name__ == "__main__":
         }
     ]
 
-    # for testCase in TEST_CASES:
-    #     head = tail = loopStartNode = None
-    #     for i in testCase["input"]:
-    #         new_node = Node(i)
-    #         if testCase["loopStart"] == i:
-    #             loopStartNode = new_node
-    #
-    #         if head is None:
-    #             head = tail = new_node
-    #         else:
-    #             tail.next = new_node
-    #             tail = new_node
-    #
-    #     if loopStartNode is not None:
-    #         tail.next = loopStartNode
-    #
-    #     output = findLoop(head)
-    #
-    #     if output == testCase["output"]:
-    #         print(f'TEST #{testCase["id"]} PASSED')
-    #     else:
-    #         print(f'TEST #{testCase["id"]} FAILED')
+    for testCase in TEST_CASES:
+        head = tail = None
+        for i in testCase["input"]:
+            new_node = Node(i)
+            if head is None:
+                head = tail = new_node
+            else:
+                tail.next = new_node
+                tail = new_node
+
+        objects = (
+            ReverseAndCompare(head),
+            IterativeApproach(head),
+        )
+
+        for obj in objects:
+            output = obj.isPalindrome()
+
+            if output == testCase["output"]:
+                print(f'TEST #{testCase["id"]} PASSED | {obj.__class__.__name__}')
+            else:
+                print(f'TEST #{testCase["id"]} FAILED | {obj.__class__.__name__}')
