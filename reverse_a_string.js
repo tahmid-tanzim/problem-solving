d136f8c335f9692c0964f1ec8662be18109871fb
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-09-02 11:39:24 +0600

    Created JavaScript Specific Directory

--- reverse_a_string.js
+++
@@ -1,7 +0,0 @@
-{
-    console.time("loop");
-    for (var i = 0; i < 1000000; i += 1){
-        // Do nothing
-    }
-    console.timeEnd("loop");
-}
--- the-hurdle-race.js
+++
@@ -1,8 +0,0 @@
-function main() {
-    var n = 5, k = 7, height = [2, 5, 4, 5, 2];
-
-    var max = Math.max(...height);
-    console.log(max > k ? max - k : 0);
-}
-
-main();
