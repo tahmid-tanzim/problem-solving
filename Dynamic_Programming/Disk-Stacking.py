eb053b8d83abfd2c2d917e56453a9f0b9ac4bdce
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-11-01 18:08:37 +0600

    AlgoExpert Disk Stacking

--- Dynamic_Programming/Disk-Stacking.py
+++ Dynamic_Programming/Disk-Stacking.py
@@ -1,5 +1,29 @@
 #!/usr/bin/python3
 # https://www.algoexpert.io/questions/Disk%20Stacking
+"""
+  You're given a non-empty array of arrays where each subarray holds three
+  integers and represents a disk. These integers denote each disk's width,
+  depth, and height, respectively. Your goal is to stack up the disks and to
+  maximize the total height of the stack. A disk must have a strictly smaller
+  width, depth, and height than any other disk below it.
+
+  Write a function that returns an array of the disks in the final stack,
+  starting with the top disk and ending with the bottom disk. Note that you
+  can't rotate disks; in other words, the integers in each subarray must
+  represent [width, depth, height] at all times.
+
+  You can assume that there will only be one stack with the greatest total
+  height.
+
+Sample Input
+disks = [[2, 1, 2], [3, 2, 3], [2, 2, 8], [2, 3, 4], [1, 3, 1], [4, 4, 5]]
+
+Sample Output
+[[2, 1, 2], [3, 2, 3], [4, 4, 5]]
+// 10 (2 + 3 + 5) is the tallest height we can get by
+// stacking disks following the rules laid out above.
+"""
+
 
 def merge(leftSortedArray, rightSortedArray):
     i = 0
@@ -84,8 +108,8 @@ def knapsack(disks, n, baseDisk, maxHeight):
         return includeBaseDisk if includeBaseDisk["max_height"] > includeNDisk["max_height"] else includeNDisk
 
 
-
 # Dynamic Programming
+# O(n^2) time | O(n) space - where n is the number of disks
 def diskStacking(disks):
     heightSortedDisks = mergeSort(disks)
     for d in heightSortedDisks:
