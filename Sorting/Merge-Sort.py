88987ad6693de626e02dcf70c9f77dc3c48a50c7
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-11-02 12:07:45 +0600

    AlgoExpert Merge Sort

--- Sorting/Merge-Sort.py
+++ Sorting/Merge-Sort.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 # https://www.algoexpert.io/questions/Merge%20Sort
+"""
+  Write a function that takes in an array of integers and returns a sorted
+  version of that array. Use the Merge Sort algorithm to sort the array.
+
+  If you're unfamiliar with Merge Sort, we recommend watching the Conceptual
+  Overview section of this question's video explanation before starting to code.
+
+Sample Input
+array = [8, 5, 2, 9, 5, 6, 3]
+
+Sample Output
+[2, 3, 5, 5, 6, 8, 9]
+"""
+
 
 def merge(leftSortedArray, rightSortedArray):
     i = 0
@@ -42,6 +56,9 @@ def mergeSortHelper(array, leftIdx, rightIdx):
     return merge(leftSortedArray, rightSortedArray)
 
 
+# Best: O(nlog(n)) time | O(n) space - where n is the length of the input array
+# Average: O(nlog(n)) time | O(n) space - where n is the length of the input array
+# Worst: O(nlog(n)) time | O(n) space - where n is the length of the input array
 def mergeSort(array):
     return mergeSortHelper(array, 0, len(array) - 1)
 
