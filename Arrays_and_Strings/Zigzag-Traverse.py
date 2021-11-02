c8dddf477d336a3b2849997856a72b9040bf0d0a
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-11-02 14:23:56 +0600

    AlgoExpert Zigzag Traverse

--- Arrays_and_Strings/Zigzag-Traverse.py
+++ Arrays_and_Strings/Zigzag-Traverse.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
+# https://www.algoexpert.io/questions/Zigzag%20Traverse
 """
-https://www.algoexpert.io/questions/Zigzag%20Traverse
+  Write a function that takes in an n x m two-dimensional array (that can be
+  square-shaped when n == m) and returns a one-dimensional array of all the
+  array's elements in zigzag order.
+
+  Zigzag order starts at the top left corner of the two-dimensional array, goes
+  down by one element, and proceeds in a zigzag pattern all the way to the
+  bottom right corner.
+
+Sample Input
+array = [
+  [1,  3,  4, 10],
+  [2,  5,  9, 11],
+  [6,  8, 12, 15],
+  [7, 13, 14, 16],
+]
+
+Sample Output
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 """
 
 
+# O(n) time | O(n) space - where n is the total number of elements in the two-dimensional array
 def zigzagTraverse(array):
     row_size = len(array)
     if row_size == 1:
@@ -74,11 +93,18 @@ def zigzagTraverse(array):
 
 
 if __name__ == '__main__':
+    # arr = [
+    #     [1, 3],
+    #     [2, 4],
+    #     [5, 7],
+    #     [6, 8],
+    #     [9, 10]
+    # ]
+
     arr = [
-        [1, 3],
-        [2, 4],
-        [5, 7],
-        [6, 8],
-        [9, 10]
+        [1, 3, 4, 10],
+        [2, 5, 9, 11],
+        [6, 8, 12, 15],
+        [7, 13, 14, 16],
     ]
     print(f'Answer - {zigzagTraverse(arr)}')
