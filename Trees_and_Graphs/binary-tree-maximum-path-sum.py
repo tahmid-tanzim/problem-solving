2ab17a44386a35b827918cf124731816791bb545
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-11-02 12:17:00 +0600

    AlgoExpert Max Path Sum In Binary Tree

--- Trees_and_Graphs/binary-tree-maximum-path-sum.py
+++ Trees_and_Graphs/binary-tree-maximum-path-sum.py
@@ -2,9 +2,33 @@
 # https://www.youtube.com/playlist?list=PL_z_8CaSLPWfxJPz2-YKqL9gXWdgrhvdn
 # https://leetcode.com/problems/binary-tree-maximum-path-sum/
 # https://www.algoexpert.io/questions/Max%20Path%20Sum%20In%20Binary%20Tree
+"""
+  Write a function that takes in a Binary Tree and returns its max path sum.
+
+  A path is a collection of connected nodes in a tree, where no node is
+  connected to more than two other nodes; a path sum is the sum of the values of
+  the nodes in a particular path.
+
+  Each BinaryTree node has an integer value, a
+  left child node, and a right child node. Children
+  nodes can either be BinaryTree nodes themselves or
+  None / null.
+
+Sample Input
+tree = 1
+    /     \
+   2       3
+ /   \   /   \
+4     5 6     7
+
+Sample Output
+18 // 5 + 2 + 1 + 3 + 7
+"""
 from TreeNode import TreeNode, create_binary_tree
 
 
+# O(n) time | O(log(n)) space
+# where n is the number of nodes in the Binary Tree
 class Solution:
     """
     0. Node value can be (-ve) negative
