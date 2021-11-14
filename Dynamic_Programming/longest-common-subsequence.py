e270a02e7acf640b0c97a84c2681acd9bb019782
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-11-14 10:42:06 +0600

    LeetCode - Print Longest Common Subsequence

--- Dynamic_Programming/longest-common-subsequence.py
+++ Dynamic_Programming/longest-common-subsequence.py
@@ -25,6 +25,7 @@ def longestCommonSubsequence(text1: str, text2: str) -> int:
     memoization_table = [[None for _ in range(len_text2)] for _ in range(len_text1)]
 
     def lcs(i, j):
+        # Base Case
         if i >= len_text1 or j >= len_text2:
             return 0
         elif memoization_table[i][j] is not None:  # Memoization
@@ -42,7 +43,7 @@ def longestCommonSubsequence(text1: str, text2: str) -> int:
 # Top Down, Memoization
 # O(m*n) time, O(m*n) space
 class Solution1:
-    def lcs(self, text1: str, text2: str, m: int, n: int, cache) -> int:
+    def _lcs(self, text1: str, text2: str, m: int, n: int, cache) -> int:
         if m < 0 or n < 0:
             return 0
 
@@ -51,23 +52,52 @@ class Solution1:
             return cache[key]
 
         if text1[m] == text2[n]:
-            cache[key] = 1 + self.lcs(text1, text2, m - 1, n - 1, cache)
+            cache[key] = 1 + self._lcs(text1, text2, m - 1, n - 1, cache)
         else:
-            cache[key] = max(self.lcs(text1, text2, m - 1, n, cache), self.lcs(text1, text2, m, n - 1, cache))
+            cache[key] = max(self._lcs(text1, text2, m - 1, n, cache), self._lcs(text1, text2, m, n - 1, cache))
 
         return cache[key]
 
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         m = len(text1) - 1
         n = len(text2) - 1
-        return self.lcs(text1, text2, m, n, dict())
+        return self._lcs(text1, text2, m, n, dict())
+
+    def _printLCSHelper(self, text1: str, text2: str, m: int, n: int, cache: dict) -> dict:
+        if m < 0 or n < 0:
+            return dict(length=0, subsequence="")
+
+        key = f"{m}:{n}"
+        if key in cache:
+            return cache[key]
+
+        if text1[m] == text2[n]:
+            UP_LEFT = self._printLCSHelper(text1, text2, m - 1, n - 1, cache)
+            length = 1 + UP_LEFT["length"]
+            subsequence = UP_LEFT["subsequence"] + text1[m]
+            cache[key] = dict(length=length, subsequence=subsequence)
+        else:
+            UP = self._printLCSHelper(text1, text2, m - 1, n, cache)
+            LEFT = self._printLCSHelper(text1, text2, m, n - 1, cache)
+            if UP["length"] > LEFT["length"]:
+                cache[key] = UP
+            else:
+                cache[key] = LEFT
+
+        return cache[key]
+
+    def printLongestCommonSubsequence(self, text1: str, text2: str) -> str:
+        m = len(text1) - 1
+        n = len(text2) - 1
+        output = self._printLCSHelper(text1, text2, m, n, dict())
+        return f"{output['length']}:{output['subsequence']}"
 
 
 # Bottom Up, Tabulation
 # O(m*n) time, O(m*n) space
 class Solution2:
     @staticmethod
-    def lcs(text1: str, text2: str, m: int, n: int, DP_TABLE) -> int:
+    def _lcs(text1: str, text2: str, m: int, n: int, DP_TABLE) -> int:
         for r in range(1, m + 1):
             for c in range(1, n + 1):
                 if text1[r - 1] == text2[c - 1]:
@@ -82,29 +112,88 @@ class Solution2:
         m = len(text1)
         n = len(text2)
         DP_TABLE = [[0 for c in range(n + 1)] for r in range(m + 1)]
-        return self.lcs(text1, text2, m, n, DP_TABLE)
+        return self._lcs(text1, text2, m, n, DP_TABLE)
+
+    @staticmethod
+    def _printLCSHelper(text1: str, text2: str, m: int, n: int, DP_TABLE) -> dict:
+        for r in range(1, m + 1):
+            for c in range(1, n + 1):
+                if text1[r - 1] == text2[c - 1]:
+                    UP_LEFT = DP_TABLE[r - 1][c - 1]
+                    length = 1 + UP_LEFT["length"]
+                    subsequence = UP_LEFT["subsequence"] + text1[r - 1]
+                    DP_TABLE[r][c] = dict(length=length, subsequence=subsequence)
+                else:
+                    UP = DP_TABLE[r - 1][c]
+                    LEFT = DP_TABLE[r][c - 1]
+                    if UP["length"] > LEFT["length"]:
+                        DP_TABLE[r][c] = UP
+                    else:
+                        DP_TABLE[r][c] = LEFT
+
+        # for r in range(m + 1):
+        #     print(DP_TABLE[r])
+        return DP_TABLE[m][n]
+
+    def printLongestCommonSubsequence(self, text1: str, text2: str) -> str:
+        m = len(text1)
+        n = len(text2)
+        DP_TABLE = [[dict(length=0, subsequence="") for c in range(n + 1)] for r in range(m + 1)]
+        output = self._printLCSHelper(text1, text2, m, n, DP_TABLE)
+        return f"{output['length']}:{output['subsequence']}"
 
 
 # Bottom Up, Tabulation
 # O(m*n) time, O(n) space
-# class Solution3:
-#     @staticmethod
-#     def lcs(text1: str, text2: str, m: int, n: int, DP_ROW_TABLE) -> int:
-#         for r in range(1, m + 1):
-#             prevColumnValue = 0
-#             for c in range(1, n + 1):
-#                 if text1[r - 1] == text2[c - 1]:
-#                     DP_ROW_TABLE[c] = 1 + DP_ROW_TABLE[c - 1]
-#                 else:
-#                     DP_ROW_TABLE[c] = max(DP_ROW_TABLE[c], prevColumnValue)
-#                 prevColumnValue = DP_ROW_TABLE[c]
-#         return DP_ROW_TABLE[n]
-#
-#     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-#         m = len(text1)
-#         n = len(text2)
-#         DP_ROW_TABLE = [0 for c in range(n + 1)]
-#         return self.lcs(text1, text2, m, n, DP_ROW_TABLE)
+class Solution3:
+    @staticmethod
+    def _lcs(text1: str, text2: str, m: int, n: int, DP_ROW_TABLE) -> int:
+        for r in range(1, m + 1):
+            prevColumnValue = 0
+            prevRowArray = DP_ROW_TABLE.copy()
+            for c in range(1, n + 1):
+                if text1[r - 1] == text2[c - 1]:
+                    DP_ROW_TABLE[c] = 1 + prevRowArray[c - 1]
+                    prevColumnValue = DP_ROW_TABLE[c]
+                else:
+                    DP_ROW_TABLE[c] = max(prevRowArray[c], prevColumnValue)
+                    prevColumnValue = DP_ROW_TABLE[c]
+            # print(DP_ROW_TABLE)
+        return DP_ROW_TABLE[n]
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        DP_ROW_TABLE = [0 for c in range(n + 1)]
+        return self._lcs(text1, text2, m, n, DP_ROW_TABLE)
+
+    @staticmethod
+    def _printLCSHelper(text1: str, text2: str, m: int, n: int, DP_ROW_TABLE) -> dict:
+        for r in range(1, m + 1):
+            prevColumnValue = dict(length=0, subsequence="")
+            prevRowArray = DP_ROW_TABLE.copy()
+            for c in range(1, n + 1):
+                if text1[r - 1] == text2[c - 1]:
+                    UP_LEFT = prevRowArray[c - 1]
+                    length = 1 + UP_LEFT["length"]
+                    subsequence = UP_LEFT["subsequence"] + text1[r - 1]
+                    DP_ROW_TABLE[c] = dict(length=length, subsequence=subsequence)
+                    prevColumnValue = DP_ROW_TABLE[c]
+                else:
+                    if prevRowArray[c]["length"] > prevColumnValue["length"]:
+                        DP_ROW_TABLE[c] = prevRowArray[c]
+                    else:
+                        DP_ROW_TABLE[c] = prevColumnValue
+                    prevColumnValue = DP_ROW_TABLE[c]
+            # print(DP_ROW_TABLE)
+        return DP_ROW_TABLE[n]
+
+    def printLongestCommonSubsequence(self, text1: str, text2: str) -> str:
+        m = len(text1)
+        n = len(text2)
+        DP_ROW_TABLE = [dict(length=0, subsequence="") for c in range(n + 1)]
+        output = self._printLCSHelper(text1, text2, m, n, DP_ROW_TABLE)
+        return f"{output['length']}:{output['subsequence']}"
 
 
 if __name__ == '__main__':
@@ -116,9 +205,18 @@ if __name__ == '__main__':
     # print(lcs_top_down_dp("abc", "abc"))  # 3
     # print(lcs_top_down_dp("abc", "def"))  # 0
 
-    obj = Solution1()
+    # obj = Solution3()
     # print(obj.longestCommonSubsequence("abcde", "ace"))  # 3
     # print(obj.longestCommonSubsequence("abc", "abc"))  # 3
     # print(obj.longestCommonSubsequence("abc", "def"))  # 0
     # print(obj.longestCommonSubsequence("bsbininm", "jmjkbkjkv"))  # 1
-    print(obj.longestCommonSubsequence("abcba", "abcbcba"))  # 5
+    # print(obj.longestCommonSubsequence("abcba", "abcbcba"))  # 5
+
+    obj = Solution3()
+    print(obj.printLongestCommonSubsequence("abcde", "ace"))  # 3
+    print(obj.printLongestCommonSubsequence("abc", "abc"))  # 3
+    print(obj.printLongestCommonSubsequence("abc", "def"))  # 0
+    print(obj.printLongestCommonSubsequence("bsbininm", "jmjkbkjkv"))  # 1
+    print(obj.printLongestCommonSubsequence("abcba", "abcbcba"))  # 5
+
+
