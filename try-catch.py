a0777aa27f53d4b6b5bc7b0e95d9e0fbe5409006
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2022-05-14 15:20:50 +0600

    Python Try Except Else

--- try-catch.py
+++ try-catch.py
@@ -1,21 +1,19 @@
-#!/usr/bin/puthon3
+#!/usr/bin/python3
 
+valid_number_range = list(range(11))
 while True:
     value = input("Enter an integer between 1 to 10: ")
     try:
         int_value = int(value)
-        if int_value < 0:
-            raise Exception("Opps, you entered negative number.")
-        elif int_value == 0:
-            raise Exception("Opps, you entered zero.")
-        elif int_value > 10:
-            raise Exception("You did not enter a number between 1 and 10!!!")
-    except ValueError:
-        print("You did not enter an integer!!!")
-    except Exception as error:
-        print(error)
-    else:
-        reciprocal_value = 1 / int_value
+        valid_int_value = valid_number_range[int_value]
+        reciprocal_value = 1 / valid_int_value
         print(f"The Reciprocal of your number is {reciprocal_value}.")
         break
+    except ValueError:
+        print("You did not enter an integer!!!")
+    except IndexError:
+        print("You did not enter a number between 1 and 10!!!")
+    except ZeroDivisionError:
+        print("Opps, you entered zero.")
     print("Please, try again.\n")
+
