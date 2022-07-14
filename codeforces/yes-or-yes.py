ce97cad454bdb022668d062d130096c584b3df99
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2022-07-14 15:13:59 +0600

    Code Forces - 71/A Way Too Long Words

--- codeforces/yes-or-yes.py
+++ codeforces/yes-or-yes.py
@@ -1,7 +1,6 @@
 # https://codeforces.com/problemset/problem/1703/A
 # A. YES or YES?
 if __name__ == "__main__":
-    o = [input().lower() for _ in range(int(input()))]
-    for i in o:
-        print('YES' if i == 'yes' else 'NO')
+    for _ in range(int(input())):
+        print('YES' if input().lower() == 'yes' else 'NO')
 
