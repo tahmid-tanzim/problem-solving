e092e59fd8d2a3b34a5b6de1fac35747cb53ebe9
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2024-07-10 00:18:47 -0600

    Coroutine timer

--- AsynchronousIO/main.py
+++ AsynchronousIO/main.py
@@ -1,14 +1,46 @@
 #!/Library/Frameworks/Python.framework/Versions/3.10/Resources/Python.app/Contents/MacOS/Python
 import asyncio
+import time
 
 
-async def main():
-    result = await asyncio.sleep(10, result='hello world')
-    print(result, '- 10 sec')
+def timeit(func):
+    if asyncio.iscoroutinefunction(func):
+        async def coroutine_wrapper(*args, **kwargs):
+            start_time = time.time()
+            coroutine_result = await func(*args, **kwargs)
+            end_time = time.time()
+            total_time = end_time - start_time
+            print(f'Coroutine Function {func.__name__!r} took {total_time:.4f} seconds')
+            return coroutine_result
+        return coroutine_wrapper
+    else:
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            non_coroutine_result = func(*args, **kwargs)
+            end_time = time.time()
+            total_time = end_time - start_time
+            # first item in the args, ie `args[0]` is `self`
+            print(f'Function {func.__name__!r} took {total_time:.4f} seconds')
+            return non_coroutine_result
+        return wrapper
+
+
+@timeit
+def power(a, b):
+    return a ** b
+
+
+@timeit
+async def main(x, y):
+    print('Start')
+    output = await asyncio.sleep(2, result=power(x, y))
+    print('Result:', output)
+    print('End')
+
 
 if __name__ == "__main__":
-    # asyncio.run(main(), debug=True)
-    with asyncio.Runner() as runner:
-        runner.run(main())
+    asyncio.run(main(3, 5), debug=True)
+
+
 
 
