ad36df9f89c61eb9827ad0f3b428f8438f85cbc9
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2024-07-10 00:38:47 -0600

    Asyncio create_task

--- AsynchronousIO/main.py
+++ AsynchronousIO/main.py
@@ -27,17 +27,25 @@ def timeit(func):
 def power(a, b):
     return a ** b
 
+
 @timeit
 async def say_after(delay, what):
     await asyncio.sleep(delay)
     print(what)
 
+
 @timeit
 async def main():
+    task1 = asyncio.create_task(say_after(3, 'hello'))
+
+    task2 = asyncio.create_task(say_after(2, 'world'))
+
     print(f"started at {time.strftime('%X')}")
 
-    await say_after(1, 'hello')
-    await say_after(2, 'world')
+    # Wait until both tasks are completed (should take
+    # around 2 seconds.)
+    await task1
+    await task2
 
     print(f"finished at {time.strftime('%X')}")
 
