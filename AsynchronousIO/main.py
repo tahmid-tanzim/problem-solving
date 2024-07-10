3b9d0c2fabc06871cc6295b1d751adfac473f980
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2024-07-09 23:30:04 -0600

    Runner context manager

--- AsynchronousIO/main.py
+++ AsynchronousIO/main.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python3
+#!/Library/Frameworks/Python.framework/Versions/3.10/Resources/Python.app/Contents/MacOS/Python
 import asyncio
 
 
 async def main():
     result = await asyncio.sleep(10, result='hello world')
-    print(result, ' - 10 sec')
+    print(result, '- 10 sec')
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    # asyncio.run(main(), debug=True)
+    with asyncio.Runner() as runner:
+        runner.run(main())
 
 
