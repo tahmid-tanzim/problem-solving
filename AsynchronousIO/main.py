a2b101d7d1ae9875e371386574542d53b802083c
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2024-07-10 00:36:58 -0600

    Asyncio run

--- AsynchronousIO/main.py
+++ AsynchronousIO/main.py
@@ -5,41 +5,45 @@ import time
 
 def timeit(func):
     if asyncio.iscoroutinefunction(func):
-        async def coroutine_wrapper(*args, **kwargs):
-            start_time = time.time()
-            coroutine_result = await func(*args, **kwargs)
-            end_time = time.time()
+        async def wrapper(*args, **kwargs):
+            start_time = time.perf_counter()
+            result = await func(*args, **kwargs)
+            end_time = time.perf_counter()
             total_time = end_time - start_time
-            print(f'Coroutine Function {func.__name__!r} took {total_time:.4f} seconds')
-            return coroutine_result
-        return coroutine_wrapper
+            print(f'Function {func.__name__!r} took {total_time:.4f} seconds')
+            return result
     else:
         def wrapper(*args, **kwargs):
-            start_time = time.time()
-            non_coroutine_result = func(*args, **kwargs)
-            end_time = time.time()
+            start_time = time.perf_counter()
+            result = func(*args, **kwargs)
+            end_time = time.perf_counter()
             total_time = end_time - start_time
-            # first item in the args, ie `args[0]` is `self`
             print(f'Function {func.__name__!r} took {total_time:.4f} seconds')
-            return non_coroutine_result
-        return wrapper
+            return result
+    return wrapper
 
 
 @timeit
 def power(a, b):
     return a ** b
 
+@timeit
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
 
 @timeit
-async def main(x, y):
-    print('Start')
-    output = await asyncio.sleep(2, result=power(x, y))
-    print('Result:', output)
-    print('End')
+async def main():
+    print(f"started at {time.strftime('%X')}")
+
+    await say_after(1, 'hello')
+    await say_after(2, 'world')
+
+    print(f"finished at {time.strftime('%X')}")
 
 
 if __name__ == "__main__":
-    asyncio.run(main(3, 5), debug=True)
+    asyncio.run(main(), debug=True)
 
 
 
